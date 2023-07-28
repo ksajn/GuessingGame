8cfb1dfe4d03a694d49ddf623881c5f63ca1ae8b
ksajn <xayngmil@gmail.com>
2023-07-28 14:07:37 +0200

    actual first commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+use colored::*;
+
 fn main() {
-    const NUMBER: i32 = 30;
+    println!("Guess number!");
+
+    let num = rand::thread_rng().gen_range(1..101);
+
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 =  match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+
+        println!("You guessed: {}", guess);
+        match guess.cmp(&num) {
+            Ordering::Less => println!("{}", "Try bigger number".red()),
+            Ordering::Greater => println!("{}", "Try smaller number".red()),
+            Ordering::Equal => {
+                println!("{}", "You won!".green());
+                break;
+            },
+        }
+    }
 }
